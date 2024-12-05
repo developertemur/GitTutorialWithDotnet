1e8d97ea8c70560e6ba7d2a9ada1bce9c3981d4c
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2024-12-05 18:22:42 +0500

    Second

--- Program.cs
+++ Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Salom dunyo");
-Console.WriteLine("add new feature 2");
-Console.WriteLine("----------------");
+Console.WriteLine("Salom dunyoasdf "); 
