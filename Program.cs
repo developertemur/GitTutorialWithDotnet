b2516e9e73f2f860ef3c8779d992df52843f51c0
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2024-12-05 18:37:59 +0500

    add 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Salom dunyo");
+<<<<<<< HEAD
 Console.WriteLine("Thirt");
+=======
+Console.WriteLine("add new feature");
+Console.WriteLine("-----------------");
+>>>>>>> e9efd83 (add 1)
